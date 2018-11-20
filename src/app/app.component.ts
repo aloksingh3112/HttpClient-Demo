fd6806ac656fb9427d5c13c2d658b9ac006c02e8
aloksingh3112 <aloksingh3112@gmail.com>
2018-11-20 23:48:41 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http-client';
+export class AppComponent implements OnInit{
+  arr;
+constructor(public appService:AppService){
+
+}
+
+ngOnInit(){
+  this.appService.getData().subscribe(data=>console.log(this.arr=data));
+}
 }
